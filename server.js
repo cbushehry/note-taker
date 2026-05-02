4b7fb521c76267b49601b52103f98505765acc6b
cbushehry <c.bushehry@gmail.com>
2026-05-01 23:40:53 -0700

    fix:replair deleteNote bug and remove console.log statements

--- server.js
+++ server.js
@@ -14,7 +14,5 @@ app.use(express.json());
 app.use('/api', apiRoutes); 
 app.use('/', htmlRoutes);  
 
-app.listen(PORT, () => {
-    console.log(`API server now on port ${PORT}!`);
-  });
+app.listen(PORT);
   
